d8dd5ae5e743692ca0b56960a242586763e4502f
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-26 21:57:44 +0530

    build an API for Deleting Issues.

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -29,3 +29,30 @@ export async function PATCH(
 
   return NextResponse.json(updated);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  if (!params.id)
+    return NextResponse.json(
+      { error: "Invalid Request, No Issue found." },
+      { status: 400 }
+    );
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json(
+      { error: "Invalid Request, No Issue found." },
+      { status: 404 }
+    );
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) },
+  });
+
+  return NextResponse.json({ success: "Successfully Deleted." });
+}
