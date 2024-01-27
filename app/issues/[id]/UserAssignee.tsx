425a112587de4fcdd06cd79ac7eba5764df93f9a
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-27 23:46:34 +0530

    populate the users data.

--- app/issues/[id]/UserAssignee.tsx
+++ app/issues/[id]/UserAssignee.tsx
@@ -1,13 +1,37 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const UserAssignee = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const { data } = await axios.get<User[]>("/api/users");
+        setUsers(data);
+      } catch (ex) {
+        setError(true);
+      }
+    };
+    fetch();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign User..." />
       <Select.Content>
-        <Select.Group>Users</Select.Group>
-        <Select.Item value="User">User 1</Select.Item>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.name!}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
       </Select.Content>
     </Select.Root>
   );
