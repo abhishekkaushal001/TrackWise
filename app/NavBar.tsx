c8203ec8342e968d61772b275e494a9508bc8b04
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-27 15:49:50 +0530

    Implementing Login & Logout.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,9 +5,13 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import React from "react";
 import classNames from "classnames";
+import { Box } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
+import Spinner from "./components/Spinner";
 
 const NavBar = () => {
   const pathName = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: "Dashboard", href: "/" },
@@ -21,19 +25,30 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={classNames({
-              "text-zinc-900": pathName === link.href,
-              "text-zinc-500": pathName !== link.href,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": pathName === link.href,
+                "text-zinc-500": pathName !== link.href,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+
+      <Box>
+        {status === "loading" && <Spinner />}
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
