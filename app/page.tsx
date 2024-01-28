2a94ff9551f7cb1369fac59db0e4208a9969acbc
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-28 23:07:19 +0530

    building Pagination component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <Pagination itemCount={100} currentPage={2} pagesize={10} />
+    </div>
+  );
 }
