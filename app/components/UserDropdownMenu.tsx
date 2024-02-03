8bbacae42af06d1e20d31e2287630af9145b7221
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-02-03 19:26:10 +0530

    Added User registration page.

--- app/components/UserDropdownMenu.tsx
+++ app/components/UserDropdownMenu.tsx
@@ -1,7 +1,6 @@
 import { PersonIcon } from "@radix-ui/react-icons";
-import { Box, DropdownMenu } from "@radix-ui/themes";
+import { DropdownMenu } from "@radix-ui/themes";
 import Link from "next/link";
-import React from "react";
 
 const UserDropdownMenu = () => {
   return (
@@ -11,8 +10,10 @@ const UserDropdownMenu = () => {
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content>
-        <DropdownMenu.Item>
-          <Link href="/register">Register</Link>
+        <DropdownMenu.Item color="gray">
+          <Link href="/register" className=" mr-3">
+            Register
+          </Link>
         </DropdownMenu.Item>
         <DropdownMenu.Item>
           <Link href="/api/auth/signin">Login</Link>
